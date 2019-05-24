98cea9badb71c57149f45c61a2de2bcd2690c176
BAANG <chcelestino@gmail.com>
2019-05-23 17:31:57 -0700

    Change jumbotron to dark w/ white text; Add target counter

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,15 @@ var losses = 0;
 var playerCount = 0; // variable for tracking player's current count
 var cpuCount = (Math.floor(Math.random() * 41 + 10)); // variable for tracking cpu's random number (10-50)
     // console.log(cpuCount + " is the cpuCount")
-var crystal1 = 1; // one crystal is always valued at 1
-    // console.log(crystal1 + " is always 1");
-var crystal2 = (Math.floor(Math.random() * 4 + 2 )); // one crystal is always valued between 2-5
-    // console.log(crystal2 + " is 2-5");
-var crystal3 = (Math.floor(Math.random() * 5 + 6 )); // one crystal is always 6-10
-    // console.log(crystal3 + " is 6-10")
-var crystal4 = (Math.floor(Math.random() * 5 + 11)); // one crystal is always 11-15
-    // console.log(crystal4 + " is 11-15")
+
+$("#cpuCount").text(cpuCount) //prints the cpuCount/target number
+
+var drink1 = 1; // one drink is always valued at 1
+    // console.log(drink1 + " is always 1");
+var drink2 = (Math.floor(Math.random() * 4 + 2 )); // one crystal is always valued between 2-5
+    // console.log(drink2 + " is 2-5");
+var drink3 = (Math.floor(Math.random() * 5 + 6 )); // one crystal is always 6-10
+    // console.log(drink3 + " is 6-10")
+var drink4 = (Math.floor(Math.random() * 5 + 11)); // one crystal is always 11-15
+    // console.log(drink4 + " is 11-15")
+var drinkArray = [drink1, drink2, drink3, drink4]; // array that stores the values of each of the four drinks
