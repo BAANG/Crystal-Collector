eac4adc082eea466b37a77f0d6f84aab80d6e9f5
BAANG <chcelestino@gmail.com>
2019-05-25 13:08:07 -0700

    Create functions for rerolling button values

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,17 +5,32 @@ var losses = 0;
 var playerCount = 0; // variable for tracking player's current count
 var cpuCount = (Math.floor(Math.random() * 41 + 10)); // variable for tracking cpu's random number (10-50)
     // console.log(cpuCount + " is the cpuCount")
-
+    
 var drink1 = 1; // one drink is always valued at 1
-    // console.log(drink1 + " is always 1");
-var drink2 = (Math.floor(Math.random() * 4 + 2 )); // one drink is always valued between 2-5
-    // console.log(drink2 + " is 2-5");
-var drink3 = (Math.floor(Math.random() * 5 + 6 )); // one drink is always 6-10
-    // console.log(drink3 + " is 6-10")
-var drink4 = (Math.floor(Math.random() * 5 + 11)); // one drink is always 11-15
-    // console.log(drink4 + " is 11-15")
-var drink5 = (Math.floor(Math.random() * 5 + 16)); // one drink is always 16-20
+// console.log(drink1 + " is always 1");
+var drink2
+var drink3
+var drink4
+var drink5
 
+
+// functions to reroll random values of drinks
+var rollDrink2 = function() { // one drink is always valued between 2-5
+    drink2 = (Math.floor(Math.random() * 4 + 2 ));
+    // console.log(drink2 + " is 2-5");
+ } 
+var rollDrink3 = function() {
+    drink3 = (Math.floor(Math.random() * 5 + 6 )); // one drink is always 6-10
+    // console.log(drink3 + " is 6-10")
+} 
+var rollDrink4 = function(){
+    (Math.floor(Math.random() * 5 + 11)); // one drink is always 11-15
+    // console.log(drink4 + " is 11-15")
+}
+var rollDrink5 = function(){
+    (Math.floor(Math.random() * 5 + 16)); // one drink is always 16-20
+    // console.log(drink5 + " is 16-20")
+}
 var drinkArray = [drink2, drink3, drink4, drink5]; // array that stores the values of each of the four drinks
 
 var customerText = [ //array of customer dialogues
