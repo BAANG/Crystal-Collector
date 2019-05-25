fc6177b56f4a5f0273d9c916d03429958af3d1a0
BAANG <chcelestino@gmail.com>
2019-05-25 13:00:52 -0700

    Define function for setting drink values

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,31 +6,6 @@ var playerCount = 0; // variable for tracking player's current count
 var cpuCount = (Math.floor(Math.random() * 41 + 10)); // variable for tracking cpu's random number (10-50)
     // console.log(cpuCount + " is the cpuCount")
 
-var customerText = [ //array of customer dialogues
-    "'Gimme something that'll make me pass out yesterday! Here's $" + cpuCount + "'",
-    "'I only have $" + cpuCount + " how many alcohols can I get with that?'",
-    "'Gimme the works!' Your customer hands you a crumpled up wad of singles worth $" + cpuCount + " Better spit in his drink.",
-    "'Can I have a water!?' This man clearly needs it because he handed you $" + cpuCount + " for something that's free. Make him something worth his money.",
-    "'It's my 21st birthday! How much alcohol is this?' They hand you $" + cpuCount + ".",
-    "'SHOTS ALL AROUND!' Your customer hands you their credit card and tells that they only have $" + cpuCount + "left until they overdraft..."
-]
-
-$(document).ready(function() { //on document load...
-var i = (Math.floor(Math.random() * (customerText.length))); //chooses random customer text
-    
-    $("#customerText").text(customerText[i]) //prints the customertext prompt with embedded random number
-    $("#cpuCount").text("$" + cpuCount)
-});
-
-
-resetGame = function() { //on game reset...
-i = (Math.floor(Math.random() * (customerText.length))); //chooses random customer text
-cpuCount = (Math.floor(Math.random() * 41 + 10)); //chooses random target number
-
-    $("#customerText").text(customerText[i]) //prints the customertext prompt with embedded random number
-}
-
-
 var drink1 = 1; // one drink is always valued at 1
     // console.log(drink1 + " is always 1");
 var drink2 = (Math.floor(Math.random() * 4 + 2 )); // one drink is always valued between 2-5
@@ -41,4 +16,39 @@ var drink4 = (Math.floor(Math.random() * 5 + 11)); // one drink is always 11-15
     // console.log(drink4 + " is 11-15")
 var drink5 = (Math.floor(Math.random() * 5 + 16)); // one drink is always 16-20
 
-var drinkArray = [drink1, drink2, drink3, drink4]; // array that stores the values of each of the four drinks
+var drinkArray = [drink2, drink3, drink4, drink5]; // array that stores the values of each of the four drinks
+
+var customerText = [ //array of customer dialogues
+    "'Gimme something that'll make me pass out yesterday! Here's $" + cpuCount + "'",
+    "'I only have $" + cpuCount + " how many alcohols can I get with that?'",
+    "'Gimme the works!' Your customer hands you a crumpled up wad of singles worth $" + cpuCount + " Better spit in his drink.",
+    "'Can I have a water!?' This man clearly needs it because he handed you $" + cpuCount + " for something that's free. Make him something worth his money.",
+    "'It's my 21st birthday! How much alcohol is this?' They hand you $" + cpuCount + ".",
+    "'SHOTS ALL AROUND!' Your customer hands you their credit card and tells that they only have $" + cpuCount + "left until they overdraft..."
+]
+
+var setDrinkValues = function(){
+    $("#drink2").attr("value", drinkArray[0])
+    $("#drink3").attr("value", drinkArray[1])
+    $("#drink4").attr("value", drinkArray[2])
+    $("#drink5").attr("value", drinkArray[3])
+}   
+
+$(document).ready(function() { //on document load...
+    var i = (Math.floor(Math.random() * (customerText.length))); //chooses random customer text
+
+        $("#customerText").text(customerText[i]) //prints the customertext prompt with embedded random number
+        $("#cpuCount").text("$" + cpuCount)
+
+    
+
+});
+
+
+resetGame = function() { //on game reset...
+i = (Math.floor(Math.random() * (customerText.length))); //chooses random customer text
+cpuCount = (Math.floor(Math.random() * 41 + 10)); //chooses random target number
+
+    $("#customerText").text(customerText[i]) //prints the customertext prompt with embedded random number
+}
+
